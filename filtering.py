cf9f33ae41507e3fd0c03712735ac7580d0dae11
mishmashi <ana.perez.bme@gmail.com>
2025-10-06 18:27:47 -0400

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -109,7 +109,7 @@ if st.session_state.index < len(questions):
     q = questions[st.session_state.index]
     q_b = questions_b[0][st.session_state.index+6]
     
-    st.write(f"**Q{st.session_state.index}:**")
+    st.write(f"**Q{st.session_state.index+1}:**")
     col1, col2, col3 = st.columns(3)
     imgstrunique = "images/"+str(st.session_state.index)+".png"
     if os.path.exists(imgstrunique):
