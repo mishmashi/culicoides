038c8c175ed37a031e949a6512ab7eb2f93afb8a
mishmashi <ana.perez.bme@gmail.com>
2025-10-06 18:03:18 -0400

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -108,7 +108,7 @@ else:
 if st.session_state.index < len(questions):
     
     q = questions[st.session_state.index]
-    q_b = questions_b[0][st.session_state.index]
+    q_b = questions_b[st.session_state.index]
     st.warning(f"qbs: {q_b}")
     
     if q_b != "" and q_b != None:
